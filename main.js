6697c3660c53b4468aa1a38a0326200d33338f0f
vencolab <41655663+vencolab@users.noreply.github.com>
2022-05-13 10:23:45 +1000

    repo  code clean up

--- main.js
+++ main.js
@@ -8,133 +8,182 @@ import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 
 // Setup
 
+let gltfFile = './models/pointcloud.glb'
+var modelScaleFactor = 1;
+
+//global declaration
+let scene; 
+
+let camera;
+
+let renderer;
+
+const canvas = document.getElementsByTagName("canvas")[0];
+
+scene = new THREE.Scene();
+
+//camera
+const fov = 60;
+
+const aspect = window.innerWidth / window.innerHeight;
+
+const near = 0.01;
+
+const far = 1000;
+
+camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+camera.position.z = 5;
+
+camera.position.x = 0;
+
+scene.add( camera );
+
+//default renderer
+renderer = new THREE.WebGLRenderer({
+
+  canvas: canvas,
+  antialias: true,
+
+});
+renderer.autoClear = false;
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
+
+renderer.setClearColor(0x000000, 0.0);
+
+//bloom renderer
+const renderScene = new RenderPass( scene, camera );
+
+const bloomPass = new UnrealBloomPass (
+
+  new THREE.Vector2 ( window.innerWidth , window.innerHeight ),
+
+  1.5,
+  0.4,
+  0.85
+
+  );
+
+  bloomPass.threshold = 0;
+
+  bloomPass.strength = .5; //intensity of glow
+
+  bloomPass.radius = 0;
+
+  const bloomComposer = new EffectComposer(renderer);
+
+  bloomComposer.setSize ( window.innerWidth, window.innerHeight );
+
+  bloomComposer.renderToScreen = true;
+
+  bloomComposer.addPass(renderScene);
+
+  bloomComposer.addPass(bloomPass);
 
 
-const scene = new THREE.Scene();
+// GLTF pointcloud LOADER
+var glbMesh = new THREE.Points();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const gltfloader = new GLTFLoader();
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+gltfloader.load( gltfFile,
+(glb) => {
+    // the request was successfull
+    let ptMaterial = new THREE.PointsMaterial({ 
+
+      color: 0xFFFFFF,
+      size: 0.0001,
+      
+     })
+
+    glbMesh = new THREE.Points( glb.scene.children[0].geometry, ptMaterial );
+
+    glbMesh.scale.set( 1 * modelScaleFactor ,1 * modelScaleFactor , 1 * modelScaleFactor );
+
+    console.log(glb);
+
+    scene.add(glbMesh);
+
+},
+(xhr) => {
+  // the request is in progress
+
+  console.log(xhr)
+
+},
+(err) => {
+  // something went wrong
+  console.error("loading .glb went wrong, ", err)
+
+})
+
+// galaxy geometry
+const starGeometry = new THREE.SphereGeometry(80, 64, 64);
+
+// galaxy material
+const starMaterial = new THREE.MeshBasicMaterial({
+
+  map: THREE.ImageUtils.loadTexture("galaxy1.png"),
+  side: THREE.BackSide,
+  transparent: true,
+
 });
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
+// galaxy mesh
+const starMesh = new THREE.Mesh( starGeometry, starMaterial );
+//starMesh.layers.set(1);
+scene.add( starMesh );
 
-renderer.render(scene, camera);
+//ambient light
+const ambientlight = new THREE.AmbientLight( 0xffffff, 0.1 );
 
-// Torus
-
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
-
-scene.add(torus);
-
-// Lights
-
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
-
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
-
-// Helpers
-
-// const lightHelper = new THREE.PointLightHelper(pointLight)
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, gridHelper)
-
-// const controls = new OrbitControls(camera, renderer.domElement);
+scene.add(ambientlight);
 
 
+//point light
+const pointLight = new THREE.PointLight( 0xffffff, 0.1 );
 
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff, emissive: 0xffffff, emissiveIntensity: 3 });
-  const star = new THREE.Mesh(geometry, material);
+pointLight.position.set( 0 , 0 , 1 );
 
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
+scene.add(pointLight);
 
-  star.position.set(x, y, z);
-  scene.add(star);
-}
+//resize listner
+window.addEventListener(
+  "resize",
+  () => {
 
-Array(200).fill().forEach(addStar);
-
-// Background
-
-const backTexture = new THREE.TextureLoader().load('Home.png');
-scene.background = backTexture;
-
-// Avatar
-
-const jeffTexture = new THREE.TextureLoader().load('jeff.png');
-
-const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
-
-scene.add(jeff);
-
-// Moon
-
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
-
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture,
-  })
+    camera.aspect = window.innerWidth / window.innerHeight;
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    bloomComposer.setSize( window.innerWidth, window.innerHeight );
+  },
+  false
 );
 
-scene.add(moon);
+// animate on scroll
+function animateOnScroll() {
 
-moon.position.z = 30;
-moon.position.setX(-10);
-
-jeff.position.z = -5;
-jeff.position.x = 2;
-
-
-
-// Scroll Animation
-
-function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
+  starMesh.rotation.y += 0.001;
+  glbMesh.rotation.y += 0.1;
 
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
-
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
 }
 
-document.body.onscroll = moveCamera;
-moveCamera();
+document.body.onscroll = animateOnScroll;
+animateOnScroll();
 
-// Animation Loop
+//orbit controller
+const controls = new OrbitControls ( camera, renderer.domElement );
 
-function animate() {
+//animation loop
+const animate = () => {
+
   requestAnimationFrame(animate);
+  controls.update();
+  bloomComposer.render();
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
-
-  moon.rotation.x += 0.005;
-
-  // controls.update();
-
-  renderer.render(scene, camera);
-}
+};
 
 animate();
